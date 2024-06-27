89ff5c8ade7e17fc8dbbb43ca04bf7e154d6d513
Katravath Vinod <katravath.vinod@globallogic.com>
2024-06-27 11:25:03 +0530

    firstCommit

---
+++ src/main/java/com/example/EmployeeApplication/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.EmployeeApplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Emp")
+public class EmployeeController {
+    @GetMapping("/Employee")
+  public  String hello()
+    {
+        return "Hello";
+    }
+
+}
